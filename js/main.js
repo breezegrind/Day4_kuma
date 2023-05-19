b3148fa089853dd0310404634c53aae550d9969a
bear <breezegrind@nifty.com>
2023-05-19 23:30:39 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function($){
+    $('.bg-switcher').bgSwitcher({
+        images:['images/bg1.jpg', 'images/bg2.jpg','images/bg3.jpg'],
+        interval:5000,
+        loop:true,
+    });
+
+    $(window).scroll(function (){
+        $(".fadein").each(function () {
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scorll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });    
+    });
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay:true,
+        dots:true,
+        centerMode:true,
+        centerPadding:'20px',
+    }); 
+
+    $('.more-button').on('click',function(){
+        if ($('.open-box').is(':hidden')){
+            $('.open-box').slideDown();
+            $(this).text('close');
+            $(this).css('background-color','#808080');
+        }
+    });
+
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function() {
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+});
